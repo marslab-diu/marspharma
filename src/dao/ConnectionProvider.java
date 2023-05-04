0dffa24c296d1a9bb98f9ca193e8ff1e7993d9bd
MarsLab <mars.lab@hotmail.com>
2023-05-05 00:30:36 +0600

    Initial commit

---
+++ src/dao/ConnectionProvider.java
@@ -0,0 +1,22 @@
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Minhazul Abedin
+ *         ID: 221-15-4919
+ */
+public class ConnectionProvider {
+    public static Connection getCon() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/marspharma?useSSL=false", "root", "1234");
+            return con;
+        } 
+        catch(Exception e) {
+            System.out.println("e");
+            return null;
+        }
+    }
+}
---
+++ src/dao/Tables.java
@@ -0,0 +1,25 @@
+package dao;
+
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Minhazul Abedin
+ *         ID: 221-15-4919
+ */
+public class Tables {
+    public static void main(String[] args) {
+        try {
+            Connection con = ConnectionProvider.getCon();
+            Statement st = con.createStatement();
+            st.executeUpdate("create table appuser(appuser_pk int AUTO_INCREMENT primary key,userRole varchar(200),name varchar(200),dob varchar(50),mobileNumber varchar(50),email varchar(200),username varchar(200),password varchar(50),address varchar(200))");
+            st.executeUpdate("insert into appuser (userRole, name, dob, mobileNumber, email, username, password, address) values('Admin','Minhazul Abedin', '28-11-2001', '8801852018502','abedin15-4919@diu.edu.bd', 'admin', 'admin', 'Mirpur, Dhaka')");
+            JOptionPane.showMessageDialog(null, "Table created successfully");
+        } 
+        catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+}
